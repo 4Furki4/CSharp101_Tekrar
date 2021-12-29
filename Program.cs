4ea41db48e8ca6d89bfc6843e5fa67acf6e3c237
Furkan Cengiz <muhammedcengiz1@gmail.com>
2021-12-29 17:48:06 +0300

    For Loop döngülerinde break ve continue öğrenildi.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// for(;;) sonsuz döngüdür.
+
+/*
+
+loop içinde 
+if(Koşul)
+    break;
+varsa, koşul sağlandığında döngü sonlanır.
+
+if(koşul)
+    continue;
+varsa, sağlanan koşulu atlar ve devam eder. 
+Mesela:
+for(int i=0; i<5; i++){
+    if(i==4)
+        continue;
+Console.Writeline(i);
+i=4 olduğunda döngüyü bir atlatıp devam edecek ve çıktı, 1235 şeklinde olacak.
+}
+break ve continue, içinde bulunduğu loop için geçerlidir!
+
+
+
+
+*/
