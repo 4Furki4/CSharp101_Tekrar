ca4f5f62f52300d42886cb04e48554831ca0367e
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-01-07 20:54:00 +0300

    Metot Tanımlama Dersinin ref kısmını çalıştım.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a=0;
+
+//Call By Value
+
+static int Topla(int a){
+a+=3;
+return a;
+
+}
+
+
+//Call By Referance
+
+static int ToplaReferance(ref int a){
+
+a+=3;
+return a;
+}
+Console.WriteLine(Topla(a));
+Console.WriteLine("Referans ile alınmadan önce a: {0}",a);
+Console.WriteLine(ToplaReferance(ref a));
+Console.WriteLine("Referans ile alındıktan sonra a: {0}",a);
+
